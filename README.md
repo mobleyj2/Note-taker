b4cbb83fda568ba49e272849b4bb728de4539978
jake <mobleyj2@gmail.com>
2023-06-12 13:37:47 -0400

    added files

---
+++ README.md
@@ -0,0 +1,61 @@
+![](https://img.shields.io/badge/License-MIT-yellow.svg)
+# Title
+  Note Taker 
+  
+# Table of Contents
+ 1. -[Title](#title)
+ 2. -[Description](#description)
+ 3. -[Lisences](#lisences)
+ 4. -[Usage](#usage)
+ 5. -[Questions](#questions)
+ 6. -[Installation](#installation)
+ 7. -[Contributions](#contributions)
+ 8. -[Test](#test)
+
+---
+
+## Description
+  this application is used to take notes and save them for later use.
+
+---
+
+## Lisences
+  MIT
+
+---
+
+## Installation
+  Install
+
+---
+
+## Usage
+  You can use this to create notes and save them for viewing later.
+
+---
+
+## Contributions
+  AskBSC, tutoring
+
+---
+
+## Test
+  
+
+---
+
+## Questions
+  N/a
+
+  
+  (http://github.com/https://github.com/mobleyj2/Note-taker.git)
+
+  mobleyj2@gmail.com
+
+  #https://drive.google.com/file/d/1y6lwiofpmr781z6Icx-dKiWuU0DZy5_k/view
+
+---
+
+
+
+
---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const allNote = require('./db/db.json');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+  res.json(allNote.slice(1));
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
+
+
+function createNewNote(body, notesArray) {
+  const newNote = body;
+  if (!Array.isArray(notesArray)) {
+    notesArray = [];
+  }
+  if (notesArray.length === 0) {
+    notesArray.push(0);
+  }
+  body.id = notesArray[0];
+  notesArray[0]++;
+  notesArray.push(newNote);
+  fs.writeFileSync(
+    path.join(__dirname, 'db', 'db.json'),
+    JSON.stringify(notesArray, null, 2)
+  );
+  return newNote;
+}
+
+app.post('/api/notes', (req, res) => {
+  const newNote = createNewNote(req.body, allNote);
+  res.json(newNote);
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+
+app.listen(PORT, () => {
+  console.log('Server listening on port', PORT);
+});
